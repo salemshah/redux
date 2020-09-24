e68f7c650e2afe58559d793bbbe3436a43e30ce3
Salem <salemshahdeve@gmail.com>
2020-09-24 16:57:08 +0200

    setup Redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from "react";
 import Counter from "./Counter";
+import {createStore} from "redux"
+
+// setup reducer
+
+const defaultValue = {
+    count: 22,
+    name: "salem"
+}
+
+function reducer(state, action) {
+    console.log(state, action)
+    if (action.type === "DECREASE") {
+        state.count = state.count - 1;
+        return state
+    }
+    return state;
+}
+
+const store = createStore(reducer, defaultValue);
+store.dispatch({type: "DECREASE"})
+store.dispatch({type: "DECREASE"})
+store.dispatch({type: "DECREASE"})
+
+//console.log(store.getState())
 const App = () => {
-  return <Counter />;
+    return <Counter state={store.getState()}/>;
 };
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Counter() {
-  return <h1>counter component</h1>;
+function Counter(props) {
+    const {count, name} = props.state
+    const [counter, setCounter] = useState(0);
+    return (
+        <div className="container text-center">
+            <h1>
+                Counter
+            </h1>
+            <div>
+                It comes from Redux: Name
+                <div className="h2">
+                    {name}
+                </div>
+            </div>
+            <div>It comes from Redux: count
+                <div className="h2">{count}</div>
+            </div>
+            <div>
+                <p>{counter}</p>
+            </div>
+            <div className="d-flex justify-content-center align-items-center">
+                <div className="mx-2">
+                    <button className="btn btn-warning"
+                            onClick={() => setCounter(counter > 0 ? counter - 1 : 0)}>Decrease
+                    </button>
+                </div>
+                <div className="mx-2">
+                    <button className="btn btn-danger" onClick={() => setCounter(0)}>Reset</button>
+                </div>
+                <div className="mx-2">
+                    <button className="btn btn-primary" onClick={() => setCounter(counter + 1)}>Increase</button>
+                </div>
+            </div>
+        </div>);
 }
 
 export default Counter;
